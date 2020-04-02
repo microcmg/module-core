f4612cd0b249526383cebeb112cf5799e4cd346d
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2020-04-02 10:22:33 +0530

    Added SEO fields.

--- common/base/Application.php
+++ common/base/Application.php
@@ -40,7 +40,14 @@ class Application {
 	public $assetsVersion;
 
 	// SEO
+	public $title;
+	public $desc;
+	public $keywords;
 	public $robots;
+	public $metas = [];
+
+	// Page
+	public $page;
 
 	public function __construct() {
 
@@ -54,6 +61,9 @@ class Application {
 		// Initialize the Application
 	}
 
+	/**
+	 * Initialize the router to identify the request URL and the corresponding page.
+	 */
 	public function initRouter() {
 
 		// Router
@@ -66,37 +76,36 @@ class Application {
 		// Clean slashes
 		$route = trim( $uri[ 0 ], '/' );
 
-		$script = empty( $route ) ? "{$this->basePath}/pages/index.php" : "{$this->basePath}/pages/{$route}.php";
+		$this->page = empty( $route ) ? "{$this->basePath}/pages/index.php" : "{$this->basePath}/pages/{$route}.php";
 
-		$script	= FileUtility::normalizePath( $script );
+		$this->page	= FileUtility::normalizePath( $this->page );
 
-		if( file_exists( $script ) ) {
+		if( !file_exists( $this->page ) ) {
 
-			include $script;
+			$this->page = "{$this->basePath}/pages/{$route}/index.php";
+
+			if( !file_exists( $this->page ) ) {
+
+				$this->page = "{$this->basePath}/pages/404.php";
+			}
+		}
+	}
+
+	/**
+	 * Page Output
+	 */
+	public function renderPage() {
+
+		$app = $this;
+
+		if( file_exists( $this->page ) ) {
+
+			include $this->page;
 		}
 		else {
 
-			$script = "{$this->basePath}/pages/{$route}/index.php";
-
-			if( file_exists( $script ) ) {
-
-				include $script;
-			}
-			else {
-
-				$script = "{$this->basePath}/pages/404.php";
-
-				if( file_exists( $script ) ) {
-
-					include $script;
-				}
-				else {
-
-					echo "Page not found.";
-				}
-			}
+			echo "Page not found.";
 		}
-
 	}
 
 }
